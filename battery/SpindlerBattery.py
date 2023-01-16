5d54a95402ce3d15e2fb0c018a3b10ea6d590775
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2023-01-17 02:12:40 +0800

    Refactor classes into folders

--- battery/SpindlerBattery.py
+++ battery/SpindlerBattery.py
@@ -1,14 +1,13 @@
 from battery import Battery
-from datetime import datetime
 
 class SpindlerBattery(Battery):
-    def __init__(self, last_service_date):
+    def __init__(self, current_date, last_service_date):
         self.last_service_date = last_service_date
-        self.current_date = datetime.today().date()
+        self.current_date = current_date
 
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date():
+        if service_threshold_date < self.current_date:
             return True
         else:
             return False
