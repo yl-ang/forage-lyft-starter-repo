acbd5e29bacbe21083f89b06dbe2ee7a9fbeac99
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2023-01-17 13:12:29 +0800

    Update Spindler service year and tests

--- test/test_car.py
+++ test/test_car.py
@@ -6,7 +6,7 @@ from car_factory import CarFactory
 class TestCalliope(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 5)
         current_mileage = 0
         last_service_mileage = 0
 
@@ -42,7 +42,7 @@ class TestCalliope(unittest.TestCase):
 class TestGlissade(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         today = datetime.today().date()
-        last_service_date = today.replace(year=today.year - 3)
+        last_service_date = today.replace(year=today.year - 5)
         current_mileage = 0
         last_service_mileage = 0
 
